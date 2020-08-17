11f70a81099ec29e7c4f7ccfa13c60d2e0747f3e
zhenya <zhenyadope@gmail.com>
2020-08-17 22:15:21 +0300

    'Создана-часть-с-вопросами-и-валидацией-ответов'

--- quiz-react/src/containers/Quiz/Quiz.js
+++ quiz-react/src/containers/Quiz/Quiz.js
@@ -1,21 +1,117 @@
 import React from 'react'
 import classes from './Quiz.module.css';
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
-
+import FinishedQuiz from "../../components/FinishedQuiz/FInishedQuiz";
 class Quiz extends React.Component {
 
     state = {
+        results:{},
+        activeQuestion: 0,
+        isFinished: false,
+        answerState: null,  // { [id]: 'success' 'error' }
         quiz: [
-
+            {
+                rightAnswerId:2,
+                question: 'Какого цвета небо?',
+                id:1,
+                answers: [
+                    {text: 'Черное',id:1},
+                    {text: 'Голубое',id:2},
+                    {text: 'Зеленое',id:3},
+                    {text: 'Белое',id:4},
+                ]
+            },
+            {
+                rightAnswerId:3,
+                question: 'В каком году основан Санкт-Питербург?',
+                id:2,
+                answers: [
+                    {text: '1803',id:1},
+                    {text: '1903',id:2},
+                    {text: '1703',id:3},
+                    {text: '1730',id:4},
+                ]
+            }
         ]
+    }
+
+    onAnswerClickHandler = answerId => {
+        if(this.state.answerState) {
+            const key = Object.keys(this.state.answerState)[0]
+            if(this.state.answerState[key] === 'success'){
+                return
+            }
+        }
+        const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
+
+        if(question.rightAnswerId === answerId) {
+            if(!results[question.id]){
+                results[question.id] = 'success'
+            }
+            this.setState({
+                answerState: {[answerId]: 'success'},
+                results
+            })
+            const timeout = window.setTimeout(()=>{
+                if(this.isQuizFinished()){
+                    this.setState({
+                        isFinished: true
+                    })
+                } else {
+                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    })
+                }
+                window.clearTimeout(timeout)
+            },1000)
+        } else {
+            results[question.id] = 'error'
+            this.setState({
+                answerState: {[answerId]: 'error'},
+                results
+            })
+        }
+
+
+    }
+
+    isQuizFinished() {
+        return this.state.activeQuestion + 1 === this.state.quiz.length
+    }
+
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {},
+        })
     }
 
     render () {
         return (
             <div className={classes.Quiz}>
                 <div className={classes.QuizWrapper}>
-                    <h1>Quiz</h1>
-                    <ActiveQuiz/>
+                    <h1>Ответьте на все вопросы</h1>
+                    {
+                        this.state.isFinished
+                        ? <FinishedQuiz
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+                            />
+                        : <ActiveQuiz
+                                answers={this.state.quiz[this.state.activeQuestion].answers}
+                                question={this.state.quiz[this.state.activeQuestion].question}
+                                onAnswerClickHandler={this.onAnswerClickHandler}
+                                quizLength={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1}
+                                state={this.state.answerState}
+                            />
+                    }
+
                 </div>
             </div>
         )
